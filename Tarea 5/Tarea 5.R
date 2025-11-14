8545d8589da3809cb93a994bdb5de65756effb1a
Karen Morales <122758347+KrenMora@users.noreply.github.com>
2025-11-14 00:06:00 -0600

    Update Tarea 5.R
    Por fin puedo mostrar con filtros de edad agresor, edad victima, victima sexo, victima escolaridad

--- Tarea 5/Tarea 5.R
+++ Tarea 5/Tarea 5.R
@@ -7,36 +7,40 @@ library(rpart.plot)
 # Cargar data
 data <- read_excel("C:/Users/krenm/Downloads/base-de-datos-violencia-intrafamiliar-ano-2024_v3.xlsx")
 
-
-data_model <- data %>%
-  select(AGR_SEXO, VIC_EDAD, VIC_ESCOLARIDAD, VIC_TRABAJA)
+data_model <- data %>% select(
+  AGR_SEXO,        
+  VIC_EDAD,
+  VIC_SEXO,
+  VIC_ESCOLARIDAD
+)
 
 # Eliminar NA
 data_model <- na.omit(data_model)
 
+data_model$AGR_SEXO <- as.factor(data_model$AGR_SEXO)
 
+data_model$EDAD30 <- ifelse(data_model$VIC_EDAD >= 30, ">=30", "<30")
+data_model$ESC_OK <- ifelse(data_model$VIC_ESCOLARIDAD != 99, "Valida", "Sin dato")
 
-data$EDAD30 <- ifelse(data$VIC_EDAD >= 30, ">=30", "<30")
-
-
-data$ESC_OK <- ifelse(data$VIC_ESCOLARIDAD != 99, "Valida", "99")
-
-
-arbol5 <- rpart(AGR_SEXO ~
-                  EDAD30 + 
-                  VIC_TRABAJA +
-                  ESC_OK ,
-                data = data, method = "class"
+arbol_sexo <- rpart(
+  AGR_SEXO ~ 
+    EDAD30 + 
+    VIC_SEXO + 
+    ESC_OK,
+  data = data_model,
+  method = "class",
+  cp = 0.01,
+  minsplit = 60,
+  maxdepth = 3
 )
 
-
 rpart.plot(
-  arbol5,
+  arbol_sexo,
   type = 2,
-  extra = 0,
+  extra = 104,
   under = TRUE,
   fallen.leaves = TRUE,
-  box.palette = "BuGn",
-  main = "Ciudad o no",
-  cex = 0.7
+  box.palette = "Blues",
+  cex = 0.7,
+  main = "Árbol de Decisión para Predecir el Sexo del Agresor"
 )
